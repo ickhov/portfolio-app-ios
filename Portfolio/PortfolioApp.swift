4c207fa2652983d1e7c3afceca461d56ce7bbd41
Iev Khov <ievkhov1@gmail.com>
2022-04-13 21:59:26 -0700

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Iev Khov on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
